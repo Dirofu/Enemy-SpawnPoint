da69588ed6868299c0d25d8656f10883b1441cd7
Dirofu <if-sd@mail.ru>
2021-04-23 15:25:53 +0700

    Init

---
+++ Assets/Scripts/GetAllSpawnPoints.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAllSpawnPoints : MonoBehaviour
+{
+    [SerializeField] private GameObject _enemy;
+    [SerializeField] private float _spawnTime;
+
+    private SpawnEnemy[] _points;
+    private int _currentPoint;
+    private float _nextSpawnTime;
+
+    private void Start()
+    {
+        _points = gameObject.GetComponentsInChildren<SpawnEnemy>();
+        _nextSpawnTime = _spawnTime;
+    }
+
+    private void Update()
+    {
+        if (_nextSpawnTime <= 0 && _currentPoint < _points.Length)
+        {
+            _points[_currentPoint].Spawn(_enemy);
+            _currentPoint++;
+            _nextSpawnTime = _spawnTime;
+        }
+        else
+        {
+            _nextSpawnTime -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    private Transform transform;
+
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
+
+    public void Spawn(GameObject enemy)
+    {
+        Instantiate(enemy, transform);
+    }
+}
