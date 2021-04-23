f093ed6baa066b1095ec795ea0bb398f5915a395
Dirofu <if-sd@mail.ru>
2021-04-23 20:55:31 +0700

    Corrected code style
    Renamed scripts

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    public void Spawn(GameObject enemy)
+    {
+        Instantiate(enemy, transform);
+    }
+}
